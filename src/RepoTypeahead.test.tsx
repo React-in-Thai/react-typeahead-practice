e4f820d1457a53f11689ed06855f842bc5f44ade
siriwatknp <siriwatkunaporn@gmail.com>
2020-11-08 13:47:24 +0700

    add empty component

---
+++ src/RepoTypeahead.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import RepoTypeahead from "./RepoTypeahead";
+
+describe("RepoTypeahead", () => {
+  it("renders without clashes", () => {
+    expect(() => render(<RepoTypeahead />)).not.toThrow();
+  });
+});
---
+++ src/RepoTypeahead.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RepoTypeahead = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default RepoTypeahead
